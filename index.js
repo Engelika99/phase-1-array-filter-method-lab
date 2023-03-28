d4c7be99ed4fbcb30b8a8ab084ba32c0f6802e88
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-28 01:01:57 -0400

    Filters arrays

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+/*function findMatching(drivers, name){
+       
+       const names = drivers.filter(value => value === name);
+       //let character = drivers.filter(char => char === name);
+       return names
+}**/
+function findMatching(drivers, attribute) {
+       return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+     }
+     
+     function fuzzyMatch(drivers, attribute) {
+       return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+     }
+     
+     function matchName(drivers, argument) {
+       return drivers.filter(function (driver) { return driver.name === argument })
+     }
